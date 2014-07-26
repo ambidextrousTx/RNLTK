106d56e734140d006a083965e55560a55e21e428
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-26 15:33:56 -0500

    Return a list of lists

--- NGrams.py
+++ NGrams.py
@@ -11,8 +11,11 @@ def generate_ngrams(text, n):
     random piece
     '''
     text_array = text.split(' ')
+    ngram_list = []
     for i in range(0, len(text_array) - n + 1):
-        print text_array[i:i + n]
+        ngram_list.append(text_array[i:i + n])
+    return ngram_list
 
-generate_ngrams('this is a random piece', 2)
-generate_ngrams('this is a random piece', 3)
+print generate_ngrams('this is a random piece', 2)
+print generate_ngrams('this is a random piece', 3)
+print generate_ngrams('this is a random piece', 4)
