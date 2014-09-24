af5f94a2787d11043865c649ab293d2ffd8e3218
ambidextrousTx <ravisinha@my.unt.edu>
2014-09-23 23:18:46 -0500

    Add tests for 2- and 4-grams

--- TestNGrams.py
+++ TestNGrams.py
@@ -9,5 +9,21 @@ class TestNGrams(unittest.TestCase):
         self.assertEqual(ngram_list, [['this'], ['is'], ['a'], ['random'],
                                       ['piece'], ['of'], ['text']])
 
+    def test_bigrams(self):
+        sentence = 'this is a random piece of text'
+        ngram_list = NGrams.generate_ngrams(sentence, 2)
+        self.assertEqual(ngram_list, [['this', 'is'], ['is', 'a'],
+                                      ['a', 'random'], ['random', 'piece'],
+                                      ['piece', 'of'], ['of', 'text']])
+
+    def test_fourgrams(self):
+        sentence = 'this is a random piece of text'
+        ngram_list = NGrams.generate_ngrams(sentence, 4)
+        self.assertEqual(ngram_list, [['this', 'is', 'a', 'random'],
+                                      ['is', 'a', 'random', 'piece'],
+                                      ['a', 'random', 'piece', 'of'],
+                                      ['random', 'piece', 'of', 'text']])
+
+
 if __name__ == '__main__':
     unittest.main()
