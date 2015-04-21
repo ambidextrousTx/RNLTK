0960aa5a1f19bcf0135ad455fc4798e2e9261430
ambidextrousTx <ravisinha@my.unt.edu>
2015-04-20 21:18:51 -0500

    Clean up tests and module for NGrams

--- src/NGrams.py
+++ src/NGrams.py
@@ -1,10 +1,13 @@
-def generate_ngrams(text, n):
+''' Methods (static for now) to do with NGram
+creation and calculations '''
+
+def generate_ngrams(text, num):
     ''' Generates all possible n-grams of a
     piece of text
 
     >>> text = 'this is a random piece'
     >>> n = 2
-    >>> generate_ngrams(text, n)
+    >>> generate_ngrams(text, num)
     this is
     is a
     a random
@@ -12,10 +15,6 @@ def generate_ngrams(text, n):
     '''
     text_array = text.split(' ')
     ngram_list = []
-    for i in range(0, len(text_array) - n + 1):
-        ngram_list.append(text_array[i:i + n])
+    for i in range(0, len(text_array) - num + 1):
+        ngram_list.append(text_array[i:i + num])
     return ngram_list
-
-print generate_ngrams('this is a random piece', 2)
-print generate_ngrams('this is a random piece', 3)
-print generate_ngrams('this is a random piece', 4)
--- test/TestNGrams.py
+++ test/TestNGrams.py
@@ -1,3 +1,5 @@
+''' Main class for testing the NGrams module
+'''
 import unittest
 import sys
 sys.path.append('../src')
@@ -5,13 +7,18 @@ import NGrams
 
 
 class TestNGrams(unittest.TestCase):
+    ''' Class to test n-gram creation methods '''
     def test_unigrams(self):
+        ''' Unigrams are just tokenized words from the
+        original string '''
         sentence = 'this is a random piece of text'
         ngram_list = NGrams.generate_ngrams(sentence, 1)
         self.assertEqual(ngram_list, [['this'], ['is'], ['a'], ['random'],
                                       ['piece'], ['of'], ['text']])
 
     def test_bigrams(self):
+        ''' Bigrams are tokens from the original text
+        taken two tokens at a time '''
         sentence = 'this is a random piece of text'
         ngram_list = NGrams.generate_ngrams(sentence, 2)
         self.assertEqual(ngram_list, [['this', 'is'], ['is', 'a'],
@@ -19,6 +26,8 @@ class TestNGrams(unittest.TestCase):
                                       ['piece', 'of'], ['of', 'text']])
 
     def test_fourgrams(self):
+        ''' 4-grams are tokens from the original text
+        taken four tokens at a time '''
         sentence = 'this is a random piece of text'
         ngram_list = NGrams.generate_ngrams(sentence, 4)
         self.assertEqual(ngram_list, [['this', 'is', 'a', 'random'],
